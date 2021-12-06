fab7dcf443a32625c6bb820bb747f31463026c92
elsisco <elsa.carlstrom@gmail.com>
2021-12-06 11:37:44 +0100

    completed monday codealong

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import express from 'express'
+import express from 'express' // Node framework to create the backend server
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import techFundings from './data/tech_fundings.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -13,12 +14,44 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const users = [
+  { id: 1, name: 'Alice', age: 33 },
+  { id: 2, name: 'Bob', age: 10 },
+  { id: 3, name: 'Daniela', age: 63 }
+]
+
 // Start defining your routes here
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
+
+// This is our first endpoint
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello browser!')
 })
 
-// Start the server
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/fundings', (req, res) => {
+  res.json(techFundings)
+})
+
+// get specific company based on id, using param
+app.get('/fundings/:index', (req, res) => {
+  const { index } = req.params
+  const companyIndex = techFundings.find((company) => company.index === +index)
+
+  if (!companyIndex) {
+    console.log('No company found!')
+    res.status(404).send('No company found with that index.') // Send just sends a string. Great for error messages.
+  } else {
+    res.json(companyIndex)
+  }
+})
+
+// Start the server (listens to the port)
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`) // Confirms that this is working
 })
