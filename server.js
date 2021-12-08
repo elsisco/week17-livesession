73ce930637829e35a9525612f636102dd4d1af4d
elsisco <elsa.carlstrom@gmail.com>
2021-12-08 12:48:06 +0100

    finished wednesday lecture codealong

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express' // Node framework to create the backend server
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
+
 import techFundings from './data/tech_fundings.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be
@@ -12,7 +14,8 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+// app.use(bodyParser.json()) // bodyParser started to deprecate some months ago
+app.use(express.json())
 
 const users = [
   { id: 1, name: 'Alice', age: 33 },
@@ -30,24 +33,73 @@ app.get('/', (req, res) => {
   res.send('Hello browser!')
 })
 
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app)) // Lists all accessible endpoints
+})
+
 app.get('/users', (req, res) => {
   res.json(users)
 })
 
+// app.get('/fundings', (req, res) => {
+//   res.json(techFundings)
+// })
+
 app.get('/fundings', (req, res) => {
-  res.json(techFundings)
+  const { company, region } = req.query
+  console.log(req.query)
+
+  let techFundingsToSend = techFundings
+
+  if (company) {
+    techFundingsToSend = techFundingsToSend.filter(
+      (item) => item.Company.toLowerCase().indexOf(company.toLowerCase()) !== -1
+    ) //Always use filter with query params
+  }
+
+  if (region) {
+    techFundingsToSend = techFundingsToSend.filter(
+      (item) => item.Region.toLowerCase().indexOf(region.toLowerCase()) !== -1
+    )
+  }
+
+  res.json({
+    response: techFundingsToSend,
+    success: true
+  })
 })
 
 // get specific company based on id, using param
-app.get('/fundings/:index', (req, res) => {
-  const { index } = req.params
-  const companyIndex = techFundings.find((company) => company.index === +index)
+app.get('/fundings/id/:id', (req, res) => {
+  // :id = path params, query params?
+  const { id } = req.params
+  const companyId = techFundings.find((company) => company.index === +id)
 
-  if (!companyIndex) {
+  if (!companyId) {
     console.log('No company found!')
-    res.status(404).send('No company found with that index.') // Send just sends a string. Great for error messages.
+    res.status(404).send('No company found with that id.') // Send just sends a string. Great for error messages. json() compiles it to a json object.
   } else {
-    res.json(companyIndex)
+    res.json(companyId)
+  }
+})
+
+app.get('/fundings/company/:company', (req, res) => {
+  const { company } = request.params //Destructuring
+
+  const companyByName = techFundings.find(
+    (item) => item.Company.toLowerCase === company.toLowerCase
+  ) //Always use find with path params
+
+  if (!companyByName) {
+    // using json() this time instead of send.
+    res.status(404).json({
+      response: 'No company found with that name',
+      success: false
+    })
+  } else {
+    res.status(200).json({
+      response: companyByName
+    })
   }
 })
 
